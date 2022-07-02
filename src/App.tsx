d3201537512603c87096eaa6e858ba9996720939
Daniel Bradac <d.bradac@email.cz>
2022-07-02 20:01:56 +0200

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-import { extend, ReactThreeFiber } from '@react-three/fiber'
 import { useState } from 'react'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Settings from './common/Settings'
 import ControlPanel from './control-panel/ControlPanelComponent'
 import { Drawable, Point, Vector } from './data-model/Drawable'
 import { Layer } from './data-model/Layer'
 import DrawerPage from './drawer/DrawerPageComponent'
 import VisualModel from './visual-components/VisualModelComponent'
-
-// Global JSX declarations
-extend({ Line_: THREE.Line })
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      orbitControls: ReactThreeFiber.Object3DNode<
-        OrbitControls,
-        typeof OrbitControls
-      >
-      line_: ReactThreeFiber.Object3DNode<THREE.Line, typeof THREE.Line>
-    }
-  }
-}
 
 const App = () => {
   const [points, setPoints] = useState<Point[]>([])
@@ -69,6 +51,8 @@ const App = () => {
       return prevSettings.copy().setAxisToggled(!prevSettings.axisToggled)
     })
   }
+
+  // Render
   return (
     <>
       <h1 className='header'>Measure 3D</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+/* eslint-disable @typescript-eslint/no-namespace */
+import { extend, ReactThreeFiber } from '@react-three/fiber'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Global JSX declarations
+extend({ Line_: THREE.Line })
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<
+        OrbitControls,
+        typeof OrbitControls
+      >
+      line_: ReactThreeFiber.Object3DNode<THREE.Line, typeof THREE.Line>
+    }
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
